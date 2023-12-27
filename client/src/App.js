4cfbbcca1658e1c1ee6c61d40e49041863dd6933
Plaxidia Hove <90321105+Plaxidia@users.noreply.github.com>
2023-12-28 00:09:35 +0200

    adding a new task and deleting task
    adding a new task and deleting task

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+//import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/tasks';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+  const fetchTasks = async () => {
+    const response = await axios.get(API_URL);
+    setTasks(response.data);
+  };
+  const addTask = async () => {
+    if (newTask.trim() !== '') {
+      const response = await axios.post(API_URL, { name: newTask });
+      setTasks([...tasks, response.data]);
+      setNewTask('');
+    }
+  };
+  const deleteTask = async (taskId) => {
+    const response = await axios.delete(`${API_URL}/${taskId}`);
+    setTasks(tasks.filter(task => task.id !== response.data.id));
+  };
+  
+
 }
 
 export default App;
